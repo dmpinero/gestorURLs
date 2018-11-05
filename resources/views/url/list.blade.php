00d91f7996dca8bfa78fb0f608f3331d35bc8930
Daniel Martínez <dmpinero@gmail.com>
2018-11-05 10:55:37 +0100

    Añadido Datatables para visualización de urls

--- resources/views/url/list.blade.php
+++ resources/views/url/list.blade.php
@@ -1,5 +1,46 @@
-@foreach($urls as $url)
-    <tr>
-        <th>{{ $url->id }}</th>
-    </tr>																		
-@endforeach
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Listado de URLs</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/css/bootstrap.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css">
+</head>
+<body>
+    <div class="container">
+        <table id="urls" class="table table-striped table-bordered" style="width:100%">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Nombre</th>
+                    <th>Descripción</th>
+                    <th>Ir a la web</th>    
+                </tr>			
+            </thead>
+            <tbody>
+                @foreach($urls as $url)
+                <tr>
+                    <td>{{ $url->id }}</td>
+                    <td>{{ $url->name }}</td>
+                    <td>{{ $url->description }}</td>
+                    <td>{{ $url->url }}</td>                                        
+                </tr>
+                @endforeach                
+            </tbody>
+        </table>
+    </div>
+    
+    
+    <script src="https://code.jquery.com/jquery-3.3.1.js"></script></script>
+    <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script></script>
+    <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
+
+    <script>
+        $(document).ready(function() {
+            $('#urls').DataTable();
+        } );        
+    </script>
+</body>
+</html>
